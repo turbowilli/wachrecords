c9373b253c74bcd8490cd123fc06775fd39707be
turbowilli <fewach@pm.me>
2026-09-21 11:47:27 +0200

    Kontakt: redundanten Hinweis entfernen

--- content/contact.js
+++ content/contact.js
@@ -4,6 +4,5 @@ const contactContent = {
   title: "Anfragen",
   intro: "Für Ausstellungen, Publikationen, Zusammenarbeit und fotografische Anfragen.",
   email: "foto@wachrecords.de",
-  note: "Die Kontaktadresse für Anfragen.",
   purchase: "Ausgewählte Fotografien und Publikationen können auf Anfrage erworben werden."
 };
